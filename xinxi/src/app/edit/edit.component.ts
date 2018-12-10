82c31ca2c1aa2133e363e0115ac19522b7e52b4a
haosiwem <45622563+haosiwem@users.noreply.github.com>
2018-12-10 13:12:45 +0800

    Update edit.component.ts

--- xinxi/src/app/edit/edit.component.ts
+++ xinxi/src/app/edit/edit.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
-import { Edit } from '../edit';
-import { Output, EventEmitter } from '@angular/core';
+import {Component, OnInit, Input, OnChanges, SimpleChanges} from '@angular/core';
+import {Edit} from '../edit';
+import {Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-edit',
@@ -11,26 +11,59 @@ export class EditComponent implements OnInit, OnChanges {
 
   @Input() public infor: Edit = new Edit();
 
-  @Output() bao = new EventEmitter();
+  @Output() save = new EventEmitter();
 
-  constructor() { }
+  public can = false;
+
+  constructor() {
+  }
 
   public ngOnChanges(changes: SimpleChanges) {
     if (changes['infor'] && changes['infor'].currentValue) {
-      this.infor = changes['infor'].currentValue;
+      this.can = false;
     }
   }
 
-  add() {
-    // tslint:disable-next-line:max-line-length
-    if (this.infor.name === '' || this.infor.sex === '' || this.infor.age === null || this.infor.tel === '' || this.infor.address === '') {
-      alert('输入不能为空');
+  onBtnAdd() {
+    this.save.emit(this.infor);
+    localStorage.setItem('information', JSON.stringify(this.infor));
+  }
+
+  onBtnCancel() {
+    const cache = JSON.parse(localStorage.getItem('information'));
+
+    if (!(cache.name === this.infor.name)) {
+      alert('您已修改数据，未保存');
+      this.onCache(cache);
+    } else if (!(cache.sex === this.infor.sex)) {
+      alert('您已修改数据，未保存');
+      this.onCache(cache);
+    } else if (!(cache.age === this.infor.age)) {
+      alert('您已修改数据，未保存');
+      this.onCache(cache);
+    } else if (!(cache.tel === this.infor.tel)) {
+      alert('您已修改数据，未保存');
+      this.onCache(cache);
+    } else if (!(cache.address === this.infor.address)) {
+      alert('您已修改数据，未保存');
+      this.onCache(cache);
     } else {
-      this.bao.emit(this.infor);
-      localStorage.setItem('information', JSON.stringify(this.infor));
+      this.onCache(cache);
     }
+  }
+
+  onCache(value) {
+    this.save.emit(value);
   }
 
   ngOnInit() {
+    const cache = localStorage.getItem('information');
+    if (!cache) {
+      this.can = true;
+    } else {
+      this.can = false;
+    }
   }
 }
+
+
